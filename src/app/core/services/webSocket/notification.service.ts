d83fa338f65d8bfe42e5b265afa4b66e62fe069b
latifaChakir <chakirlatifa2001@gmail.com>
2025-03-12 13:18:12 +0000

    ajouter notification apres la publication de event

---
+++ src/app/core/services/webSocket/notification.service.ts
@@ -0,0 +1,67 @@
+import { Injectable } from '@angular/core';
+import SockJS from 'sockjs-client';
+import { Client, Stomp } from '@stomp/stompjs';
+import { Subject } from 'rxjs';
+import { UserService } from '../user/user.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotificationService {
+  private stompClient!: Client;
+  private notificationSubject = new Subject<string>();
+  private currentUserId!: number;
+
+  constructor(private userService: UserService) {
+    this.userService.getCurrentUser().subscribe(user => {
+      this.currentUserId = user.id;
+      this.connect();
+    });
+  }
+
+  private connect() {
+    const token = localStorage.getItem("token");
+    console.log('JWT Token:', token);
+
+    const socket = new SockJS('http://localhost:9091/ws');
+
+    this.stompClient = new Client({
+      webSocketFactory: () => socket,
+      debug: (msg) => console.log(msg),
+      connectHeaders: {
+        Authorization: `Bearer ${token}`
+      },
+      beforeConnect: () => {
+        console.log("Préparation à la connexion avec le token");
+      },
+      onConnect: () => {
+        this.subscribeToNotifications();
+        console.log('✅ WebSocket connecté pour les notifications');
+      },
+      onStompError: (error) => {
+        console.error('❌ Erreur WebSocket :', error);
+      }
+    });
+
+    this.stompClient.activate();
+  }
+
+  private subscribeToNotifications() {
+    this.stompClient.subscribe('/topic/events', (message) => {
+      console.log('📩 Notification brute reçue:', message);
+      try {
+        const notificationData = message.body;
+        console.log('✅ Notification après parsing:', notificationData);
+        this.notificationSubject.next(notificationData);
+      } catch (error) {
+        console.error('❌ Erreur lors du parse de la notification:', error);
+      }
+    });
+
+    console.log('✅ Abonné au canal de notifications');
+  }
+
+  getNotifications() {
+    return this.notificationSubject.asObservable();
+  }
+}
--- src/app/layouts/navbar/navbar.component.ts
+++ src/app/layouts/navbar/navbar.component.ts
@@ -1,19 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
+import {NotificationService} from "../../core/services/webSocket/notification.service";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
-  constructor(private router:Router) {
+export class NavbarComponent implements OnInit{
+  notifications: string[] = [];
+  constructor(private router:Router, private notificationService: NotificationService) {
   }
   logout(): void {
     console.log('Logging out...');
     localStorage.removeItem('token');
     this.router.navigate(['/']);
+  }
+  ngOnInit() {
+    this.notificationService.getNotifications().subscribe(notification => {
+      this.notifications.push(notification);
+    });
   }
 }
