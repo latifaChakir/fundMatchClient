81abc0768e3c49cca6aabfebb1d1e60cf863812e
latifaChakir <chakirlatifa2001@gmail.com>
2025-03-16 03:14:52 +0000

    regler de probleme de role

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,3 +1,4 @@
+// register.component.ts
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup} from "@angular/forms";
 import {Store} from "@ngrx/store";
@@ -10,25 +11,48 @@ import {RegisterRequest} from "../../core/models/auth/Register-request.model";
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent implements OnInit{
+export class RegisterComponent implements OnInit {
   registerForm!: FormGroup;
-  constructor(private store : Store,private fb: FormBuilder) {
+  showRoleSelection = false;
+  selectedRole = 1;
+
+  constructor(private store: Store, private fb: FormBuilder) {
   }
+
   ngOnInit() {
     this.registerForm = createRegisterValidator(this.fb);
   }
 
+  onRegisterClick() {
+    if (this.registerForm.valid) {
+      this.showRoleSelection = true;
+    }
+  }
+
   onSubmit() {
+    if (!this.showRoleSelection) {
+      this.showRoleSelection = true;
+      return;
+    }
+
     const formValues = this.registerForm.getRawValue();
-    const user : RegisterRequest = {
-      firstName : formValues.firstName,
-      lastName : formValues.lastName,
-      email : formValues.email,
-      password : formValues.password,
-      phoneNumber : formValues.phoneNumber,
-      isActive : true,
-      roles : [1]
+    const user: RegisterRequest = {
+      firstName: formValues.firstName,
+      lastName: formValues.lastName,
+      email: formValues.email,
+      password: formValues.password,
+      phoneNumber: formValues.phoneNumber,
+      isActive: true,
+      roles: [this.selectedRole]
     }
     this.store.dispatch(AuthActions.registerUser({ user }))
+  }
+
+  selectRole(roleId: number) {
+    this.selectedRole = roleId;
+  }
+
+  goBack() {
+    this.showRoleSelection = false;
   }
 }
--- src/app/investor/create-account/create-account.component.ts
+++ src/app/investor/create-account/create-account.component.ts
@@ -1,14 +1,16 @@
 import {Component, OnInit} from '@angular/core';
-import {Observable} from "rxjs";
+import {Observable, of} from "rxjs";
 import {Sector} from "../../core/models/sector/sector.model";
 import {FormBuilder, FormGroup} from "@angular/forms";
 import {Store} from "@ngrx/store";
 import {selectSectors} from "../../core/stores/sector/sector.reducer";
-import {map} from "rxjs/operators";
+import {map, switchMap, take} from "rxjs/operators";
 import {SectorActions} from "../../core/stores/sector/sector.actions";
 import {InvestorActions} from "../../core/stores/investor/investor.actions";
 import {Investor} from "../../core/models/investor/investor.model";
 import {createInvestor} from "../../core/validators/investor/investor";
+import {selectFilteredInvestors} from "../../core/stores/investor/investor.reducer";
+import {UserService} from "../../core/services/user/user.service";
 
 @Component({
   selector: 'app-create-account',
@@ -17,31 +19,70 @@ import {createInvestor} from "../../core/validators/investor/investor";
 })
 export class CreateAccountComponent implements OnInit{
   sectors$: Observable<Sector[]>;
+  investors$: Observable<Investor[]>;
+  private currentUserId!: number;
+  error: string | null = null;
+  success: string | null = null;
+  selectedInvestor$: Observable<Investor | null> = of(null);
   preferredGeographies: string[] = [
     "Afrique", "Amérique du Nord", "Amérique du Sud",
     "Asie", "Europe", "Océanie", "Moyen-Orient"
   ];
   investorForm: FormGroup;
   sectorsData$: Observable<any[]>;
+  existingInvestor: Investor | null = null;
 
-  constructor(private fb: FormBuilder, private store: Store) {
+  constructor(private fb: FormBuilder, private store: Store, private userService: UserService) {
     this.sectors$ = this.store.select(selectSectors);
+    this.investors$ = this.store.select(selectFilteredInvestors);
     this.investorForm = createInvestor(this.fb);
     this.sectorsData$ = this.sectors$.pipe(
       map(sectors => sectors.map(sector => ({ id: sector.id, text: sector.name })))
     );
-
   }
 
   ngOnInit() {
     this.store.dispatch(SectorActions.loadSectors());
+    this.store.dispatch(InvestorActions.loadInvestors());
+    this.userService.getCurrentUser().subscribe(user => {
+      this.currentUserId = user.id;
+
+      this.selectedInvestor$ = this.investors$.pipe(
+        map(investors => investors.find(investor => investor.user?.id === this.currentUserId) || null)
+      );
+
+      this.selectedInvestor$.pipe().subscribe(investor => {
+        if (investor) {
+          this.existingInvestor = investor;
+          this.loadInvestorData(investor);
+        }
+      });
+    });
+  }
+  loadInvestorData(investor: Investor) {
+    this.investorForm.patchValue({
+      basic: {
+        organization: investor.organization,
+        location: investor.location,
+        contactInfo: investor.contactInfo
+      },
+      investment: {
+        minInvestment: investor.minInvestment,
+        maxInvestment: investor.maxInvestment,
+        investmentType: investor.investmentType,
+        experienceYears: investor.experienceYears,
+        averageInvestmentsPerYear: investor.averageInvestmentsPerYear,
+        investmentStrategy: investor.investmentStrategy,
+        preferredGeographies: investor.preferredGeographies,
+        sectors: investor.sectors?.map(sector => sector.id) || []
+      }
+    });
   }
 
-
-  submit() {
+  saveProfile() {
     if (this.investorForm.valid) {
       const formValues = this.investorForm.getRawValue();
-      const investor: Investor = {
+      const investorData: Investor = {
         organization: formValues.basic.organization,
         location: formValues.basic.location,
         contactInfo: formValues.basic.contactInfo,
@@ -52,13 +93,29 @@ export class CreateAccountComponent implements OnInit{
         averageInvestmentsPerYear: formValues.investment.averageInvestmentsPerYear,
         investmentStrategy: formValues.investment.investmentStrategy,
         preferredGeographies: formValues.investment.preferredGeographies,
-        sectors: formValues.investment.sectors.map((id: number) => ({ id }))
+        sectors: formValues.investment.sectors.map((id: number) => ({ id })),
       };
 
-      this.store.dispatch(InvestorActions.addInvestor({ investor }));
-      this.investorForm.reset();
+      if (this.existingInvestor) {
+        const updatedInvestor = {
+          ...this.existingInvestor,
+          ...investorData,
+          id: this.existingInvestor.id
+        };
+        this.store.dispatch(InvestorActions.updateInvestor({ investor: updatedInvestor }));
+        this.success="Your Profile has been updated successfully.";
+      } else {
+        this.store.dispatch(InvestorActions.addInvestor({ investor: investorData }));
+        this.success="Your Profile has been created successfully.";
+
+      }
     } else {
       console.log("Formulaire invalide", this.investorForm.errors);
+      this.error="Error invalide";
     }
+  }
+
+  submit() {
+    this.saveProfile();
   }
 }
